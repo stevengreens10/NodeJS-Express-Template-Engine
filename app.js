0835f880ad9368765528529ac4ec30d79c3223e0
stgreen <stgreen@ncsu.edu>
2018-01-03 13:04:32 -0500

    Static files, partial views, POST request parsing

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var routes = require('./routes/index')
+var path = require('path')
 
 var app = express()
 
 app.set('port', (process.env.PORT || 3000))
+
+// Sets the template engine to EJS
 app.set('view engine', 'ejs')
 
+// Parses POST requests and stores form data in req.body.(name)
+app.use(bodyParser.urlencoded({extended:false}));
+
+// Allows access to files in the public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Moves routing to routes/index.js
 app.use('/', routes)
+
+
+
 
 app.listen(app.get('port'), function() {
     console.log("Server running on port " + app.get('port'))
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,20 @@
 var express = require('express')
+
 var router = express.Router()
 
 router.get('/', function(req, res){
     res.render('index', {
         random: Math.random()
     })
+})
+
+router.get('/form', function(req, res){
+    res.render('form')
+})
+
+router.post('/form', function(req, res){
+    console.log(req.body)
+    res.render('form')
 })
 
 module.exports = router
