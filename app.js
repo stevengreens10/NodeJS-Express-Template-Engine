aed8203ba280412aa0c476b9954276e670ac8c14
stgreen <stgreen@ncsu.edu>
2018-01-03 13:06:45 -0500

    Test

--- app.js
+++ app.js
@@ -19,9 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Moves routing to routes/index.js
 app.use('/', routes)
 
-
-
-
 app.listen(app.get('port'), function() {
     console.log("Server running on port " + app.get('port'))
 })
